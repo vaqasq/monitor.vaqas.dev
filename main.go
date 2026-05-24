64fb10872ffbfb24971649ef8c6ef35b85ab81a4
HealingAura32 <vqudus@gmail.com>
2026-05-24 00:59:50 -0400

    added struct

--- main.go
+++ main.go
@@ -15,6 +15,18 @@ func main() {
 func fetchContainerList() bool {
 
 	containerListAPI := os.Getenv("CONTAINER_LIST_API")
+
+	type Health struct {
+		Status        string `json:"Status"`
+		FailingStreak int    `json:"FailingStreak"`
+	}
+
+	type Resp struct {
+		State  string `json:"State"`
+		Status string `json:"Status"`
+		Health Health `json:"Health"`
+	}
+
 	resp, err := http.Get(containerListAPI)
 
 	if err != nil {
@@ -33,6 +45,8 @@ func fetchContainerList() bool {
 func fetchLiveContainerMetrics() bool {
 
 	liveStreamMetricsAPI := os.Getenv(("LIVE_STREAM_METRICS_API"))
+
+	// Insert STRUCT for the specific data you want to receive.
 
 	resp, err := http.Get(liveStreamMetricsAPI)
 
