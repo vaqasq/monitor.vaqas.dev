74f35b28a5db083e457eff2468d4ec6fb3ed5844
HealingAura32 <vqudus@gmail.com>
2026-05-23 23:58:54 -0400

    iteration 1

--- main.go
+++ main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
 func main() {
+
+}
+
+func fetchContainerList() bool {
+
+	containerListAPI := os.Getenv("CONTAINER_LIST_API")
+	resp, err := http.Get(containerListAPI)
+
+	if err != nil {
+		fmt.Printf("Error while retrieving ContainerList: %v", err)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	if strings.Contains(contentType, "application/json") {
+		return true
+	}
+
+	return false
+
+}
+
+func fetchLiveContainerMetrics() bool {
+
+	liveStreamMetricsAPI := os.Getenv(("LIVE_STREAM_METRICS_API"))
+
+	resp, err := http.Get(liveStreamMetricsAPI)
+
+	if err != nil {
+		fmt.Printf("Error whiel retrieving LiveContainerMetrics: %v ", err)
+	}
+
+	contentType := resp.Header.Get("Content-Type")
+	if strings.Contains(contentType, "application/json") {
+		return true
+	}
+
+	return false
 
 }
